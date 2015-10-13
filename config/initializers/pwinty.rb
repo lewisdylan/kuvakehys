7743c8b6d9c3e0a46e2e7097d3f1191cee1a66f8
Michael Bumann <michael@railslove.com>
2015-10-13 14:47:31 +0200

    added FakePwinty API to be used in dev/test mode

--- config/initializers/pwinty.rb
+++ config/initializers/pwinty.rb
@@ -1,1 +1,20 @@
-PWINTY = Pwinty.client(api_version: 'v2.2', merchant_id: ENV['PWINTY_MERCHANT_ID'], api_key: ENV['PWINTY_API_KEY'], production: ENV['PWINTY_PRODUCTION'] == 'true')
+if Rails.env.production?
+  PWINTY = Pwinty.client(api_version: 'v2.2', merchant_id: ENV['PWINTY_MERCHANT_ID'], api_key: ENV['PWINTY_API_KEY'], production: ENV['PWINTY_PRODUCTION'] == 'true')
+else
+  class FakePwinty
+    def create_order(args)
+      {'id' => SecureRandom.hex(3)}
+    end
+    def add_photos(*args)
+      true
+    end
+    def get_order_status(id)
+      {'isValid' => (id.to_i%2 == 0)}
+    end
+    def update_order_status(id, status)
+      true
+    end
+  end
+  PWINTY = FakePwinty.new
+end
+
