02cf3b61f881c960ae53ff047e65f772211228d6
Michael Bumann <michael@railslove.com>
2015-10-13 18:00:34 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012215934) do
+ActiveRecord::Schema.define(version: 20151013113840) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +56,14 @@ ActiveRecord::Schema.define(version: 20151012215934) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "picture_fingerprint"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "phone"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
