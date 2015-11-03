7e740d2a1e191d031765e621376e451e2ebb8af1
Michael Bumann <michael@railslove.com>
2015-11-03 10:55:04 +0200

    catch telegrammer boot error if internet is down
    Telegrammer does an API request on boot. This fails when the internet
    is down. We simply ignore that when developing offline.

--- config/initializers/messengers.rb
+++ config/initializers/messengers.rb
@@ -1,4 +1,9 @@
 Ongair.configure {|c| c.token = ENV['ONGAIR_TOKEN'] } if ENV['ONGAIR_TOKEN']
 
-TELEGRAM = Telegrammer::Bot.new(ENV['TELEGRAM_TOKEN']) if ENV['TELEGRAM_TOKEN']
+begin
+  TELEGRAM = Telegrammer::Bot.new(ENV['TELEGRAM_TOKEN']) if ENV['TELEGRAM_TOKEN']
+rescue Exception => e
+  Rails.logger.error("telgeram can not be initialized. Internet down?")
+  Rails.logger.error(e.message)
+end
 
