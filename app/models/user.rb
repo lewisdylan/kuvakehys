443f24494e6df8db7cdc5051ae215fd272963223
Michael Bumann <michael@railslove.com>
2015-11-09 22:55:19 +0200

    filter only users with email addresses to send inactivity notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ActiveRecord::Base
 	has_many :photos, dependent: :nullify
 
   scope :inactive, -> { where(['last_import_at IS NOT NULL and last_import_at < ?', 2.weeks.ago]) }
+  scope :with_email, -> { where('email IS NOT NULL') }
 
   def self.notify_inactive_users
-    User.inactive.where(['(last_inactivity_notifier_at IS NULL OR last_inactivity_notifier_at < ?) AND (inactivity_notifications_count IS NULL OR inactivity_notifications_count < ?)', 2.weeks.ago, 4]).find_each do |user|
+    User.with_email.inactive.where(['(last_inactivity_notifier_at IS NULL OR last_inactivity_notifier_at < ?) AND (inactivity_notifications_count IS NULL OR inactivity_notifications_count < ?)', 2.weeks.ago, 4]).find_each do |user|
       UserMailer.inactivity_notification(user).deliver_now
       user.update_attributes(last_inactivity_notifier_at: Time.now, inactivity_notifications_count: user.inactivity_notifications_count.to_i + 1)
     end
